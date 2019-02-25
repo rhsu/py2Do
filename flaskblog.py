56e4bd93108f1c3236d3df73e329e304b7b1291b
Robert Hsu <robert@easypost.com>
2019-02-25 15:55:18 -0800

    styling

--- flaskblog.py
+++ flaskblog.py
@@ -6,36 +6,40 @@ app = Flask(__name__)
 
 @app.route("/")
 def dontmatter():
-	return "hello world"
+    return "hello world"
 
 
 @app.route("/test")
 def test():
-	response = { "foo" : "bar", "baz": "bill" }
-	return jsonify(response)
+    response = {
+        "foo": "bar",
+        "baz": "bill"
+    }
+    return jsonify(response)
 
 
 @app.route("/something", methods=['POST'])
 def something():
-	return jsonify(request.json)
+    return jsonify(request.json)
 
 
 @app.route("/register", methods=['POST'])
 def register():
-	#
-	# TBD import ipdb; ipdb.set_trace()
-	#
-	#import ipdb; ipdb.set_trace()
+    #
+    # TBD import ipdb; ipdb.set_trace()
+    #
+    # import ipdb; ipdb.set_trace()
 
-	request_json = request.json
-	
-	username = request_json['username']
-	password = request_json['password']
+    request_json = request.json
+    
+    username = request_json['username']
+    password = request_json['password']
 
-	service = RegisterService(username, password)
-	service.perform
+    service = RegisterService(username, password)
+    service.perform
 
-	return jsonify(request.json)
+    return jsonify(request.json)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
